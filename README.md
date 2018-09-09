48982f172d9d0c8544ff2e9f8ecbea61f1bd92e0
Henri Tuhola <henri.tuhola@gmail.com>
2018-09-10 00:47:50 +0300

    simple forms for procedures

--- README.md
+++ README.md
@@ -17,6 +17,7 @@ The backend target of this project is Javascript.
 ## Directory contents
 
  * lexing.py - For sample program inputs and other such things.
+ * forms.py - Formats and structured used in the system.
 
 ## Related papers and texts
 
@@ -24,3 +25,22 @@ The backend target of this project is Javascript.
 "Horn-Clauses as an Intermediate Representation for Program Analysis and Transformation"
  * [Delimited continuations](https://en.wikipedia.org/wiki/Delimited_continuation)
 
+## Errors made during development
+
+I've kept a small log of errors I've done.
+This won't probably be a complete list of all the errors,
+but it may be interesting to see where somebody is doing errors.
+
+In the sequence of checking equality between variables, the index
+was not checked correctly.
+
+        same_procedure = (self.procedure == other.procedure)
+        same_op        = (self.operation == other.operation)
+        same_index     = (self.operation == other.operation)
+
+Consequence of the error: The program gave wrong equality between variables.
+Detected: Review after writing the code.
+
+In the same operation: We incorrectly checked equality between procedures,
+although this operation is used to check that two clauses have an
+equivalent prefix.
---
+++ forms.py
@@ -0,0 +1,114 @@
+def demonstration():
+    a = Variable()
+    b = Variable()
+    ret = Variable()
+    bn = Variable()
+    gcd_a = Procedure('gcd', [a, b], [ret], [
+        Operation("!=", [b, Constant(0)], []),
+        Operation("mod", [a, b], [bn]),
+        Operation("gcd", [b, bn], [ret]),
+    ])
+    gcd_a.introduce()
+
+    a = Variable()
+    b = Variable()
+    gcd_b = Procedure('gcd', [a, b], [a], [
+        Operation("==", [b, Constant(0)], []),
+    ])
+    gcd_b.introduce()
+
+    assert all_eq(gcd_a.inputs, gcd_b.inputs)
+    assert is_complementary(gcd_a.body[0], gcd_b.body[0]) 
+
+class Procedure(object):
+    def __init__(self, label, inputs, outputs, body):
+        self.label = label
+        self.inputs = inputs
+        self.outputs = outputs
+        self.body = body
+
+    def introduce(self):
+        i = 0
+        for inp in self.inputs:
+            inp.introduce(self, 0, i)
+            i += 1
+        k = 1
+        for operation in self.body:
+            operation.introduce(self, k)
+            k += 1
+
+class Operation(object):
+    def __init__(self, name, inputs, outputs):
+        self.name = name
+        self.inputs = inputs
+        self.outputs = outputs
+
+    def introduce(self, procedure, operation):
+        i = 0
+        for output in self.outputs:
+            output.introduce(procedure, operation, i)
+            i += 1
+
+class Constant(object):
+    def __init__(self, value):
+        self.value = value
+
+    def eq(self, other):
+        if not isinstance(other, Constant):
+            return False
+        return self.value == other.value
+
+class Variable(object):
+    def __init__(self):
+        self.procedure = None
+        self.operation = 0
+        self.index     = 0
+
+    def introduce(self, procedure, operation, index):
+        if self.procedure is not None:
+            match  = (self.procedure == procedure)
+            match &= (self.operation == operation)
+            match &= (self.index == index)
+            assert match, "variable is being reused"
+        self.procedure = procedure
+        self.operation = operation
+        self.index = index
+
+    def eq(self, other):
+        if not isinstance(other, Variable):
+            return False
+        same_op        = (self.operation == other.operation)
+        same_index     = (self.index == other.index)
+        return same_op and same_index
+
+def all_eq(xs, ys):
+    if not len(xs) == len(ys):
+        return False
+    for i in range(len(xs)):
+        if not xs[i].eq(ys[i]):
+            return False
+    return True
+
+# Some operations are complementary to each other.
+# This means that if operation succeeds,
+# then the complementary operation with same inputs will fail.
+complementary_operations = {
+    "==": "!=",
+    "<":  ">=",
+    "<=": ">",
+}
+
+def is_complementary_name(a, b):
+    if complementary_operations.get(a, None) == b:
+        return True
+    if complementary_operations.get(b, None) == a:
+        return True
+    return False
+
+def is_complementary(a, b):
+    if not is_complementary_name(a.name, b.name):
+        return False
+    return all_eq(a.inputs, b.inputs)
+
+if __name__=='__main__':
+    demonstration()
